315ea5deb6365d1300d246159610fedcf415ace7
mingrammer <mingrammer@gmail.com>
2018-04-21 01:13:03 +0900

    Separate darwin/linux version and windows version for flog

--- flog_windows.go
+++ flog_windows.go
@@ -1,5 +1,3 @@
-// +build darwin linux
-
 package main
 
 import (
@@ -9,18 +7,15 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 )
 
 // Run checks overwrite flag and generates logs with given options
 func Run(option *Option) error {
 	logDir := filepath.Dir(option.Output)
-	oldMask := syscall.Umask(0000)
 	if err := os.MkdirAll(logDir, 0766); err != nil {
 		return err
 	}
-	syscall.Umask(oldMask)
 	if _, err := os.Stat(option.Output); err == nil && !option.Overwrite {
 		return errors.New(option.Output + " already exists. You can overwrite with -w option")
 	}

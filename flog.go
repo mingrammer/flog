65bb7081aa1cfe842c8fa8963c7091e4dd58dcd1
mingrammer <mingrammer@gmail.com>
2018-04-21 00:47:43 +0900

    Set the POSIX umask to 0000 when make the writable directory

--- flog.go
+++ flog.go
@@ -7,15 +7,18 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
 
 // Run checks overwrite flag and generates logs with given options
 func Run(option *Option) error {
 	logDir := filepath.Dir(option.Output)
-	if err := os.MkdirAll(logDir, 0666); err != nil {
+	oldMask := syscall.Umask(0000)
+	if err := os.MkdirAll(logDir, 0766); err != nil {
 		return err
 	}
+	syscall.Umask(oldMask)
 	if _, err := os.Stat(option.Output); err == nil && !option.Overwrite {
 		return errors.New(option.Output + " already exists. You can overwrite with -w option")
 	}

3dc4c02f670e7294844e3fecbf3211142f23ff7a
mingrammer <mingrammer@gmail.com>
2018-04-23 00:58:31 +0900

    Fix an example test for NewLog function

--- flog_test.go
+++ flog_test.go
@@ -15,20 +15,17 @@ func ExampleNewLog() {
 	monkey.Patch(time.Now, func() time.Time { return stopped })
 	defer monkey.Unpatch(time.Now)
 
-	fmt.Println(NewLog("apache_common", 0))
-	// Output: 222.83.191.222 - Kozey7157 697 [2018-04-22T09:30:00Z] "DELETE /innovate/next-generation" 302 24570
-
-	fmt.Println(NewLog("apache_combined", 0))
-	// Output: 222.83.191.222 - Kozey7157 119 [2018-04-22T09:30:00Z] "DELETE /innovate/next-generation" 302 81317 "https://www.forwardholistic.biz/mission-critical/synergize/morph/sticky" "Mozilla/5.0 (Windows NT 5.01) AppleWebKit/5320 (KHTML, like Gecko) Chrome/40.0.875.0 Mobile Safari/5320"
-
-	fmt.Println(NewLog("apache_error", 0))
-	// Output: [2018-04-22T09:30:00Z] [quia:crit] [pid 4214:tid 6037] [client: 90.151.9.107] If we back up the program, we can get to the SSL sensor through the redundant SAS program!
-
-	fmt.Println(NewLog("rfc3164", 0))
-	// Output: 2018-04-22T09:30:00Z Daniel2872 info-mediaries[9030]: I'Ll reboot the auxiliary USB protocol, that should sensor the SAS capacitor!
-
-	fmt.Println(NewLog("unknown", 0))
+	fmt.Print(NewLog("apache_common", 0))
+	fmt.Print(NewLog("apache_combined", 0))
+	fmt.Print(NewLog("apache_error", 0))
+	fmt.Print(NewLog("rfc3164", 0))
+	fmt.Print(NewLog("unknown", 0))
 	// Output:
+	// 222.83.191.222 - Kozey7157 697 [2018-04-22T09:30:00Z] "DELETE /innovate/next-generation" 302 24570
+	// 174.144.199.149 - Mosciski7760 386 [2018-04-22T09:30:00Z] "GET /networks/revolutionary" 204 70360 "https://www.directeyeballs.org/streamline" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7) AppleWebKit/5312 (KHTML, like Gecko) Chrome/37.0.821.0 Mobile Safari/5312"
+	// [2018-04-22T09:30:00Z] [tempore:warn] [pid 7401:tid 4039] [client: 184.155.77.136] Try to synthesize the SMS capacitor, maybe it will compress the online program!
+	// 2018-04-22T09:30:00Z Wolff5542 Centralized[5680]: Quantifying the capacitor won't do anything, we need to parse the neural SAS program!
+	//
 }
 
 func TestNewSplitFileName(t *testing.T) {

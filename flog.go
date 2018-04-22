17ed03243cfcf27beaa1bfb347616944280d2adc
mingrammer <mingrammer@gmail.com>
2018-04-22 15:14:33 +0900

    Extract only OS specific functions

--- flog.go
+++ flog.go
@@ -1,31 +1,13 @@
-// +build darwin linux
-
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 )
-
-// Run checks overwrite flag and generates logs with given options
-func Run(option *Option) error {
-	logDir := filepath.Dir(option.Output)
-	oldMask := syscall.Umask(0000)
-	if err := os.MkdirAll(logDir, 0766); err != nil {
-		return err
-	}
-	syscall.Umask(oldMask)
-	if _, err := os.Stat(option.Output); err == nil && !option.Overwrite {
-		return errors.New(option.Output + " already exists. You can overwrite with -w option")
-	}
-	return Generate(option)
-}
 
 // Generate generates the logs with given options
 func Generate(option *Option) error {

ad248088bd8c374d850f64dc736d854efa74acc6
mingrammer <mingrammer@gmail.com>
2020-04-01 15:53:38 +0900

    chore: bump up to v0.4.1

--- option.go
+++ option.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/pflag"
 )
 
-const version = "0.3.1"
+const version = "0.4.1"
 const usage = `flog is a fake log generator for common log formats
 
 Usage: flog [options]
